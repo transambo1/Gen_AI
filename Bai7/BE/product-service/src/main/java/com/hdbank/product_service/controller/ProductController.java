1cb87776295641735a61a399b83e481f5c72cf09
Trm-xnh <lamthanh51124@gmail.com>
2026-04-08 16:53:34 +0700

    feat: done product and discovery

---
+++ Bai7/BE/product-service/src/main/java/com/hdbank/product_service/controller/ProductController.java
@@ -0,0 +1,34 @@
+package com.hdbank.product_service.controller;
+
+import com.hdbank.product_service.dto.ProductRequest;
+import com.hdbank.product_service.model.Product;
+import com.hdbank.product_service.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/product")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductRepository productRepository;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createProduct(@RequestBody ProductRequest productRequest) {
+        Product product = Product.builder()
+                .name(productRequest.getName())
+                .description(productRequest.getDescription())
+                .price(productRequest.getPrice())
+                .build();
+        productRepository.save(product);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+}
